7d860166ddc7707d70adc7eb1fbc1584e80a50ba
Loic Lachiver <llachiver@emi.u-bordeaux1.fr>
2019-11-28 15:06:34 +0100

    added UnitProd

---
+++ src/picrom/entity/UnitProd.java
@@ -0,0 +1,28 @@
+package picrom.entity;
+
+import java.util.LinkedList;
+
+import picrom.entity.unit.Unit;
+
+public class UnitProd{
+	
+	private Unit currentUnit;
+	private int nbUnit;
+	
+	private boolean levelingUp;
+	
+	private int nbTurnProd;
+	private int turnCount;
+	
+	public UnitProd(Unit u, int nbU, int nbTurnProd, int turnCount) {
+		this.u = u;
+		this.nbU = nbU;
+		this.nbTurnProd = nbTurnProd;
+		this.turnCount = turnCount;
+	}
+	
+	
+	
+	
+	
+}
