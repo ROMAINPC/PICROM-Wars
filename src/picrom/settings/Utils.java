6ce7935dc5dda3e6d17052ca0f233102b14b1957
picachoc <loic.lachiver@gmail.com>
2019-12-05 15:59:52 +0100

    added Kingdom Enum + owner constructor

--- src/picrom/settings/Utils.java
+++ src/picrom/settings/Utils.java
@@ -1,9 +1,22 @@
 package picrom.settings;
 
+import java.util.Random;
+
 public class Utils {
+	private static Random r = Settings.SEED;
 
 	public static double map(double value, double start, double stop, double targetStart, double targetStop) {
 		return targetStart + (targetStop - targetStart) * ((value - start) / (stop - start));
+	}
+
+	public static String generateKingdomName() {
+		String name = "";
+		String alphabet = "aaaazeeeertyuuuuiiiioooopqsdfghjklmwxcvbn";
+		for (int i = 0; i < r.nextInt(5) + 4 ; i++) {
+			name += i == 0 ? Character.toUpperCase(alphabet.charAt(r.nextInt(alphabet.length())))
+					: alphabet.charAt(r.nextInt(alphabet.length()));
+		}
+		return "Kingdom of " + name;
 	}
 
 }
