9cc6b348949cca7b3a42a1af0b1058fbf7e61737
picachoc <loic.lachiver@gmail.com>
2019-12-05 17:14:34 +0100

    Kingdom attribution fix

--- src/picrom/settings/Kingdom.java
+++ src/picrom/settings/Kingdom.java
@@ -15,38 +15,41 @@ public enum Kingdom {
 	VATICAN("Papal States",Color.YELLOW),
 	PERSIAN("Persian Empire",Color.GREEN),
 	MONGOL("Mongol Empire",Color.rgb(204, 255, 255)),
+	ISLAMICSTATE("Islamic state",Color.BLACK),
 	//When it is undefined, a random name and color will be generated.
 	UNDEFINED("",Color.BLACK);
 	
 	private String name;
 	private Color color;
-	private static int nbPlayers = 0;
-	
-	private static final List<Kingdom> VALUES = Collections.unmodifiableList(Arrays.asList(values()));
-	private static final int SIZE = VALUES.size();
+	private static int index = 0;
+
+	private static List<Kingdom> kingdoms = Arrays.asList(values());
+	private static final int SIZE = kingdoms.size();
 	private static final Random r = Settings.SEED;
-	
-	Kingdom(String name, Color color){
+
+	Kingdom(String name, Color color) {
 		this.name = name;
 		this.color = color;
 	}
-	
+
 	public String getName() {
-		if(this != UNDEFINED)
+		if (this != UNDEFINED)
 			return name;
 		return Utils.generateKingdomName();
 	}
-	
+
 	public Color getColor() {
-		if(this != UNDEFINED)
+		if (this != UNDEFINED)
 			return color;
 		return Color.hsb(r.nextInt(360), 1.0, 1.0);
 	}
 
 	public static Kingdom randomKingdom() {
-		if(nbPlayers != VALUES.size()) {
-			nbPlayers++;
-			return VALUES.get(r.nextInt(SIZE));}
-		return UNDEFINED;
+		if (index == 0)
+			Collections.shuffle(kingdoms);
+		if (index == kingdoms.size())
+			return UNDEFINED;
+		index++;
+		return kingdoms.get(index - 1);
 	}
 }
