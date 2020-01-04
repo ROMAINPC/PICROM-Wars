849ca00173676f86dcf9a6a812b1589dd3201f2a
romainpc <romainpc.lechat@laposte.net>
2020-01-04 20:22:47 +0100

    Add AI for Neutrals Castles

--- src/picrom/owner/Neutral.java
+++ src/picrom/owner/Neutral.java
@@ -1,8 +1,15 @@
 package picrom.owner;
 
+import java.util.Random;
+
 import picrom.entity.castle.Castle;
+import picrom.entity.unit.Knight;
+import picrom.entity.unit.Pikeman;
+import picrom.utils.Settings;
 
 public class Neutral extends Owner implements Pensive {
+
+	private static Random random = Settings.SEED;
 
 	public Neutral() {
 		super("NE");
@@ -17,10 +24,44 @@ public class Neutral extends Owner implements Pensive {
 		// Because Neutrals produce Units really slowly, they have all their time to
 		// make money and so don't need to improve their Castles.
 
+		// Because Neutrals don't have ambitions, they will not produce siege units
+
+		// When this code was writed, Pikeman have the best ratio : production time /
+		// Health points. So if the castle is attacked, then Pikeman will be produce in
+		// priority.
+
 		for (Castle castle : this.getCastles()) {
-			// TODO Choose best current production for defend the castles (need to know time
-			// left
-			// to not cancel and restart production at each call)
+			boolean underAttack = false;
+			for (Owner ennemy : castle.getContext().getOwners()) {
+				if (ennemy != this) {
+					for (Castle ennemyCastle : ennemy.getCastles()) {
+						if (ennemyCastle.getObjective() == castle) {
+							underAttack = true;
+							break;
+						}
+					}
+				}
+				if (underAttack)
+					break;
+			}
+
+			if (castle.getProductionTimeLeft() == 0) {
+				if (underAttack) {
+					castle.setProduction(Pikeman.class);
+				} else {
+					int r = random.nextInt(2);
+					if (r == 0) {
+						castle.setProduction(Knight.class);
+					} else if (r == 1) {
+						castle.setProduction(Pikeman.class);
+					}
+				}
+
+			} else {
+				if (underAttack && castle.getProduction() != Pikeman.class) {
+					castle.setProduction(Pikeman.class);
+				}
+			}
 		}
 	}
 }
